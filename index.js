d3db0cece4198051f808c4061094dffe84e46b05
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-06-29 23:18:33 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -1119,7 +1119,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!")
     } catch (error) {
         console.error("MongoDB connection error:", error);
